45e6c473e1d537b38d372ecc1edc40e9fb5aa14f
jiten228 <jiten228@yahoo.com>
2020-12-24 15:23:12 -0600

    Change in Master

--- src/com/company/Kitten.java
+++ src/com/company/Kitten.java
@@ -8,6 +8,8 @@ public class Kitten {
 public static void bugfix(){
     System.out.println("Bugfix method added to local master");
     System.out.println("Change in Master");
+    System.out.println("fs;f;skf;skf;skf");
+    System.out.println("bbbbbbbbbbbbbbbbbb");
 
 }
 public static void feature4(){
