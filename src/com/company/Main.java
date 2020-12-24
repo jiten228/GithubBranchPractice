69241f65c192c2646421c9f5f6fd676b5883e2db
jiten228 <jiten228@yahoo.com>
2020-12-24 12:36:29 -0600

    Bugfix on local master

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,4 +6,7 @@ public class Main {
 	// write your code here
         System.out.println("Welcome to new project of Github Branch practice");
     }
+    public static void bugfix(){
+        System.out.println("bugfix method added in local master");
+    }
 }
