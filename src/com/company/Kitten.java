d0d1b1987dff0d4f6b95085a2a8e6dc8bc59fc68
jiten228 <jiten228@yahoo.com>
2020-12-24 13:23:30 -0600

    Kitten .java

---
+++ src/com/company/Kitten.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public class Kitten {
+    public static void main(String[] args) {
+
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Welcome to new project of Github Branch practice");
+        System.out.println("It's time to Commit and push");
     }
 }
